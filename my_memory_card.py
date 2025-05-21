bfc2f432468b08b627b053870a43bfa1b46584a4
AdriUrtis <aortiz2@maristes.alumne.cat>
2025-05-21 20:01:01 +0200

    Update my_memory_card.py

--- my_memory_card.py
+++ my_memory_card.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel)
 from random import shuffle
-
+#Class Question
 class Question():
     def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
         self.question = question
